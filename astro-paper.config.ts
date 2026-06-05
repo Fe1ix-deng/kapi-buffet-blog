87db153ffcf3b18eaaab80ee7169e01be01c8920
apple <apple@appledeMacBook-Pro.local>
2026-06-05 15:52:29 +0800

    Add homepage keyword search

--- astro-paper.config.ts
+++ astro-paper.config.ts
@@ -3,7 +3,7 @@ import { defineAstroPaperConfig } from "./src/types/config";
 export default defineAstroPaperConfig({
   site: {
     url: "https://blog.020023.xyz",
-    title: "卡皮巴菲特笔记大全",
+    title: "Kapi Fund",
     description: "可爱又迷人的正派角色",
     author: "Kapi buffet",
     profile: "https://satna.ing",
@@ -30,6 +30,11 @@ export default defineAstroPaperConfig({
   },
   socials: [
     { name: "github", url: "https://github.com/Fe1ix-deng/kapi-buffet-blog" },
+    {
+      name: "xiaohongshu",
+      url: "https://www.xiaohongshu.com/user/profile/6143b19a000000001f0366ca?xsec_token=AB82CW6O1jSZhpkud7D873cxGlY5aV3FVbu0iA55xfrmU=&xsec_source=pc_note",
+      linkTitle: "Kapi Fund 的小红书主页",
+    },
   ],
   shareLinks: [
     { name: "whatsapp", url: "https://wa.me/?text=" },
